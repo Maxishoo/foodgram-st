a8d1b9ceedbb747c0ac36bfb50c5f3fc2e5a1d7b
Max <rcmaksim@gmail.com>
2025-05-28 23:13:28 +0300

    Add actions15

--- backend/api/tests.py
+++ backend/api/tests.py
@@ -1,5 +1,4 @@
 from django.contrib.auth import get_user_model
-from django.urls import reverse
 from django.test import TestCase
 from django.core.files.uploadedfile import SimpleUploadedFile
 from rest_framework import status
